dd464210093e1857cbb80967447220b449fc9822
Valerii Stavitskii <v.s@leadsdoit.io>
2021-09-13 11:37:14 +0300

    add api chat method

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::post('chat/send-message', [App\Http\Controllers\ChatController::class, 'sendMessage'])->name('sendMessage');
